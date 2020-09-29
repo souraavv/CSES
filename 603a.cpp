a20cc482c6f9735089a5e093cd44ddedf77d3b99
Sourav Sharma <souravsharma2468@gmail.com>
2020-09-29 21:11:27 +0530

    Add files via upload

---
+++ 603a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define hey(x) cerr << "[" #x << " : " << x << "]\n";
+#define int long long int
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpi vector<pair<int, int> >
+#define vvpi vector<vector<pair<int, int> > >
+#define all(v) (v).begin(), (v).end()	
+#define rall(v) (v).rbegin(), (v).rend()
+#define pii pair<int, int>
+#define pb push_back
+#define SZ(x) (int)(x).size()
+#define inf 1e12
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define bpc(x) __builtin_popcountll(x)
+#define Cout cout << fixed << setprecision(12)
+#define fill(v, val) memset((v), val, sizeof(v))
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define hey2(x, y) cerr << "[" << #x << " : " << x << "], [" << #y << " : " << y << "]\n";
+
+const int N = 1e6 + 10;
+const int mod = 1e9 + 7;
+const double pi = acos(-1);
+
+void print(vi &a, int start = 0) {
+	cerr << "Print\n[";
+	for(int i = start; i < SZ(a); ++i) {
+		cerr << a[i] << " ]"[i == SZ(a) - 1];
+	}
+	cerr << "\n";
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+    
+	return 0;
+}
