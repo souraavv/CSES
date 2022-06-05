5d1abb680324d2d6249e3074b17f167cd98a8875
Sourav Sharma <souravsharma2468@gmail.com>
2022-06-05 18:46:12 +0530

    Added Explanation

--- AtCoderDP/P-IndependentSet.cpp
+++ AtCoderDP/P-IndependentSet.cpp
@@ -1,40 +1,96 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-const int MAXN = 1e5;
-const int MOD = 1e9 + 7;
+#define int long long int
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpi vector<pair<int, int> >
+#define vvpi vector<vector<pair<int, int> > >
+#define all(v) (v).begin(), (v).end()    
+#define rall(v) (v).rbegin(), (v).rend()
+#define pii pair<int, int>
+#define pb push_back
+#define SZ(x) (int)(x).size()
+#define inf 1e16
+#define F first
+#define S second
+#define bpc(x) __builtin_popcountll(x)
+#define Cout cout << fixed << setprecision(12)
+#define fast ios::sync_with_stdio(false); cin.tie(0);
 
-int add(int i, int j) {
-	if ((i += j) >= MOD)
-		i -= MOD;
-	return i;
+const int mod = 1e9 + 7;
+const double pi = acos(-1);
+
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template<typename ...Args>
+void logger(string vars, Args&&... values) {
+    cout << "[";
+    cout << vars << "] = [";
+    string delim = "";
+    (..., (cout << delim << values, delim = ", "));
+    cout << "]\n";
 }
-int mul(int i, int j) {
-	return int((long long) i * j % MOD);
+
+void print(vi &a, int start = 0) {
+    int n = a.size();
+    for(int i = start; i < n; ++i) cerr << a[i] << ' ';
+    cerr << "\n";
 }
 
-vector<int> adj[MAXN];
-int dp[MAXN][2];
-
-void dfs(int i, int p) {
-	dp[i][0] = dp[i][1] = 1;
-	for (int j : adj[i]) if (j != p) {
-		dfs(j, i);
-		dp[i][0] = mul(dp[i][0], dp[j][1]);
-		dp[i][1] = mul(dp[i][1], add(dp[j][0], dp[j][1]));
-	}
+void print2D(vvi& a) {
+    int n = a.size();
+    for (int i = 0; i < n; ++i) print(a[i]);
 }
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
+int add(int a, int b) {
+    a += b;
+    if (a >= mod) {
+        a -= mod;
+    }
+    return a;
+}
 
-	int n;
-	cin >> n;
-	for (int i = 0, a, b; i + 1 < n; ++i) {
-		cin >> a >> b, --a, --b;
-		adj[a].push_back(b), adj[b].push_back(a);
-	}
-	dfs(0, 0);
-	cout << add(dp[0][0], dp[0][1]) << '\n';
+int mult(int a, int b) {
+    a %= mod;
+    b %= mod;
+    return (a * b) % mod;
+}
+void dfs(vector<vector<int> >& g, vector<vector<int> >& ways, int u, int par = -1) {
+    ways[u][0] = ways[u][1] = 1;
+    for (int child: g[u]) {
+        if (child != par) {
+            dfs(g, ways, child, u);
+            ways[u][1] = mult(ways[u][1], add(ways[child][0], ways[child][1]));
+            ways[u][0] = mult(ways[u][0], ways[child][1]);
+        }
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    const int mxN = 1e5 + 10;
+    int N;
+    cin >> N;
+    vector<vector<int> > g(mxN + 1);
+    for (int i = 0; i + 1 < N; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    // defines number of ways we can color a given tree root at i, if we fill 0 -> black or 1-> white
+    // such that no adjacent have black color 
+    // Ans is simple for the leaf nodes
+    
+    vector<vector<int> > ways(mxN + 1, vector<int>(2, 0));
+    vector<bool> vis(mxN + 1, false);
+    dfs(g, ways, 0);
+    int totalWays = 0;
+    totalWays = add(ways[0][0], ways[0][1]);
+    cout << totalWays << '\n';
+
+    return 0;
 }
